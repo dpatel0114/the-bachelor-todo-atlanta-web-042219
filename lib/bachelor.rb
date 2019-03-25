ac16c3ae2bee188e70676f1b560250b0416ee626
Dhara Patel <1991dharapatel@gmail.com>
2019-03-25 15:37:46 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,8 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
-  data.each do 
+  data.each do |occupation|
+    if occupation == 
 end
 
 def count_contestants_by_hometown(data, hometown)
