8016c484fe6162a93d4e703ed5c7e5517f0a0dda
Dhara Patel <1991dharapatel@gmail.com>
2019-03-25 15:30:35 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,7 +6,7 @@ def get_first_name_of_season_winner(data, season)
   #   end 
   # end
 
-  season.each do |data|
+  hash[season].each do |data|
     if "status" == "Winner"
       return name.split.first 
     end
