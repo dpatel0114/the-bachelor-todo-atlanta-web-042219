6b3476ef07592b619ac0933dfff22591fc1e28df
Dhara Patel <1991dharapatel@gmail.com>
2019-03-25 15:19:43 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,10 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  for main_key in hash.keys
-    if main_key = season 
-      hash[:main_key][:name]
-    end 
-  end
+  # for main_key in hash.keys
+  #   if main_key = season 
+  #     hash[:main_key][:name]
+  #   end 
+  # end
   
     
 end
