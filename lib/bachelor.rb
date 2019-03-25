6be08b620be12c72e3d1d82bf7880aae96bff12f
Dhara Patel <1991dharapatel@gmail.com>
2019-03-25 15:42:46 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,32 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  # for main_key in hash.keys
+  #   if main_key = season 
+  #     hash[:main_key][:name]
+  #   end 
+  # end
+
+  # hash[season].each do |data|
+  #   if data["status"] == "Winner"
+  #     return name.split.first 
+  #   end
+  # end
+  
+  data[season].each do |acter| 
+    if acter["status"] == "Winner"
+      return acter["name"].split.first
+    end
+  end
+  
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |actor|
+    if actor ["occupation"] == occupation
+      return actor["naem"]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
